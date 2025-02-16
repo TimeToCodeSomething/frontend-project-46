1db562c0c4572908e0d665662797c39e4ebf4031
poshlikushat <88384205+TimeToCodeSomething@users.noreply.github.com>
2025-02-16 17:54:26 +0700

    task 2

--- gendiff.js
+++ gendiff.js
@@ -5,4 +5,8 @@ const program = new Command();
 program
     .name('gendiff')
     .version('1.0.0')
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>');
+
+program.parse(process.argv);
