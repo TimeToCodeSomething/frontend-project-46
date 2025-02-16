1435d671d114370cdfb869f150849426685695f2
poshlikushat <88384205+TimeToCodeSomething@users.noreply.github.com>
2025-02-16 15:55:01 +0700

    first task

---
+++ gendiff.js
@@ -0,0 +1,8 @@
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+    .name('gendiff')
+    .version('1.0.0')
+    .description('Compares two configuration files and shows a difference.');
