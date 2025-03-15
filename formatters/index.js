e6db8e6f3e58f70bd5195ae8c04446b1bdbaccee
poshlikushat <88384205+TimeToCodeSomething@users.noreply.github.com>
2025-03-15 21:23:59 +0700

    added json.js

--- formatters/index.js
+++ formatters/index.js
@@ -1,9 +1,11 @@
 import plain from './plain.js';
 import formatDiff from '../formatDiff.js'; // если требуется другой формат, например, "stylish"
+import json from './json.js';
 
 const formatters = {
 		plain,
 		stylish: formatDiff, // уже реализованный форматер
+		json,
 };
 
 export default (diffTree, formatName = 'stylish') => {
